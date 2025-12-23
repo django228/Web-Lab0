b39b1b267944d9774b7dea976819a0e437d3630a
django228 <d123kbudim@gmail.com>
2025-12-23 19:17:19 +0300

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Lab0/',
+  base: '/Web-Lab0/',
   plugins: [
     react({
       babel: {
